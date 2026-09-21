0c9f7bf3b0a80ebe4804fd7aecea38be2791f5b3
Shishir Singh <singhshishir156@gmail.com>
2026-09-21 22:34:36 +0530

    Fix button command references in processbutton.py

--- processbutton.py
+++ processbutton.py
@@ -5,8 +5,8 @@ def processCancel():
     print('Cancel button is clicked')
 
 window = Tk()
-btOK = Button(window, text='OK', fg='red', bg='black', command='processok')
-btcancel = Button(window, text='cancel', fg='green', command='processCancel')
+btOK = Button(window, text='OK', fg='red', bg='black', command= processok)
+btcancel = Button(window, text='cancel', fg='green', command= processCancel)
 
 btOK.pack()
 btcancel.pack()
